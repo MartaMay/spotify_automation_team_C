fe1da7c7089ce775b5a799c56f501b32fd333bf1
Marta Halushko <mhalushko@aol.com>
2024-03-25 19:43:03 -0400

    MBranchCommit10

--- src/test/java/tests/NewUserRegistration.java
+++ src/test/java/tests/NewUserRegistration.java
@@ -4,9 +4,11 @@ import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.ProfilePage;
 import utilities.CSVReader;
 import utilities.ConfigReader;
 import utilities.Driver;
@@ -18,7 +20,10 @@ public class NewUserRegistration extends TestBase {
     public void newUserSignUp(String email, String password, String name, String day, String year) throws InterruptedException {
 
         Driver.getDriver().get(ConfigReader.getProperty("url"));
+
+        logger.info("Navigate to the Registration Page");
         Driver.getDriver().findElement(By.xpath("//button[text()='Sign up']")).click();
+        logger.info("Provide registration credentials (username, email, password etc) from the data provider");
         Driver.getDriver().findElement(By.id("username")).sendKeys(email);
         Driver.getDriver().findElement(By.xpath("//span[text()='Next']")).click();
         Driver.getDriver().findElement(By.id("new-password")).sendKeys(password);
@@ -33,8 +38,12 @@ public class NewUserRegistration extends TestBase {
         Driver.getDriver().findElement(By.xpath("//label[@for='gender_option_prefer_not_to_say']")).click();
         Driver.getDriver().findElement(By.xpath("//span[text()='Next']")).click();
         Driver.getDriver().findElement(By.xpath("//span[text()='Sign up']")).click();
-        Driver.getDriver().findElement(By.cssSelector("[class='Button-sc-1dqy6lx-0 gCEYvk encore-over-media-set SFgYidQmrqrFEVh65Zrg']")).click();
+        logger.info("Log in with the registered credentials");
+        new ProfilePage().clickProfile();
         Driver.getDriver().findElement(By.xpath("//span[text()='Profile']")).click();
+
+        logger.info("Confirm that profile name matching the name entered on registration page");
+        Assert.assertEquals(name, Driver.getDriver().findElement(By.xpath("//h1[@class='Text__TextElement-sc-if376j-0 ksSRyh encore-text-headline-large']")).getText());
 
 
 
